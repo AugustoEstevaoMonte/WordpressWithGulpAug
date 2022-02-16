f5200768fca0e7e7d9dd1b0baa5076fd5cc848f1
Augusto Estevão Monte <augusto-estevao@hotmail.com>
2022-02-16 14:56:14 -0300

    Gulp wordpress template
    Template para otimizar seu desenvolvimento em wordpress

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="title" content="portfolioAugusto">
+	<meta name="description" content="Um site de portfólio do desenvolvedor Augusto Estevão">
+	<meta name="keywords" content="portfólio, desenvolvedor,estágio,php,wordpress,vtex,vtexio">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="Portuguese">
+	<meta name="author" content="Augusto Estevão Monte">
+	<title>Portfólio do Augusto</title>
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/vendor/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/dist/style.css" />
+	<script defer src="<?php bloginfo('template_url'); ?>/dist/script.js"></script>
+</head>
+
+<body>
+	<h1>Hello World.exe</h1>
+	
+	<!--Scripts do bootstrap-->
+	<script src="/vendor/bootstrap.min.js"></script>
+
+</body>
+
+</html>
